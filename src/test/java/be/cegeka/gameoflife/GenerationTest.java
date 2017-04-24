081627d859882e9cca4a189835bf315f271ece97
Tim Schraepen <sch3lp@gmail.com>
2017-04-24 21:39:56 +0200

    started Generation test to get neighbours for a given position

---
+++ src/test/java/be/cegeka/gameoflife/GenerationTest.java
@@ -0,0 +1,11 @@
+package be.cegeka.gameoflife;
+
+import org.junit.Test;
+
+public class GenerationTest {
+
+    @Test
+    public void getNeighbours_NoCellAtPosition_ReturnsEmptyList() throws Exception {
+
+    }
+}
