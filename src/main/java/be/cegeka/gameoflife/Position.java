a72ab6189d7f9e788d9277c8d687f379f5fa6ecd
Tim Schraepen <sch3lp@gmail.com>
2017-04-24 23:04:00 +0200

    shuffled methods around for easier readability

--- src/main/java/be/cegeka/gameoflife/Position.java
+++ src/main/java/be/cegeka/gameoflife/Position.java
@@ -7,13 +7,13 @@ public class Position {
     private final int x;
     private final int y;
 
-    public static Position pos(int x, int y){
-        return new Position(x, y);
-    }
-
     private Position(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public static Position pos(int x, int y){
+        return new Position(x, y);
     }
 
     public int x() {
@@ -22,20 +22,6 @@ public class Position {
 
     public int y() {
         return y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Position position = (Position) o;
-        return x == position.x &&
-                y == position.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
     }
 
     public Position left() {
@@ -53,4 +39,19 @@ public class Position {
     public Position bottom() {
         return pos(x + 1, y);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
 }
