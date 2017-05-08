181b646054f9e1d0320200d638895898f44d8bc5
pieter-1983 <pieter.sterckx@cegeka.com>
2017-05-08 09:50:54 +0200

    Refactored getNumberOfAliveNeighbours to make it  more understandable

--- src/main/java/be/cegeka/gameoflife/World.java
+++ src/main/java/be/cegeka/gameoflife/World.java
@@ -29,30 +29,62 @@ public class World {
 
     public int getNumberOfAliveNeighbours(int row, int column) {
         int numberOfAliveNeighbours = 0;
-        if (row > 0 && column > 0 && getGrid()[row - 1][column - 1].isAlive()) {
+        if (upperLeftCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (row > 0 && getGrid()[row - 1][column].isAlive()) {
+        if (upperCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (row > 0 && column != ammountOfRowsAndColumns - 1 && getGrid()[row - 1][column + 1].isAlive()) {
+        if (upperRightCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (column > 0 && getGrid()[row][column - 1].isAlive()) {
+        if (leftCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (column != ammountOfRowsAndColumns - 1 && getGrid()[row][column + 1].isAlive()) {
+        if (rightCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (column > 0 && row != ammountOfRowsAndColumns - 1 && getGrid()[row + 1][column - 1].isAlive()) {
+        if (bottomLeftCellExistsAndIsALive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (row != ammountOfRowsAndColumns - 1 && getGrid()[row + 1][column].isAlive()) {
+        if (bottomCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
-        if (row != ammountOfRowsAndColumns - 1 && column != ammountOfRowsAndColumns - 1 && getGrid()[row + 1][column + 1].isAlive()) {
+        if (botoomRightCellExistsAndIsAlive(row, column)) {
             numberOfAliveNeighbours += 1;
         }
         return numberOfAliveNeighbours;
+    }
+
+    private boolean botoomRightCellExistsAndIsAlive(int row, int column) {
+        return row != ammountOfRowsAndColumns - 1 && column != ammountOfRowsAndColumns - 1 && getGrid()[row + 1][column + 1].isAlive();
+    }
+
+    private boolean bottomCellExistsAndIsAlive(int row, int column) {
+        return row != ammountOfRowsAndColumns - 1 && getGrid()[row + 1][column].isAlive();
+    }
+
+    private boolean bottomLeftCellExistsAndIsALive(int row, int column) {
+        return column > 0 && row != ammountOfRowsAndColumns - 1 && getGrid()[row + 1][column - 1].isAlive();
+    }
+
+    private boolean rightCellExistsAndIsAlive(int row, int column) {
+        return column != ammountOfRowsAndColumns - 1 && getGrid()[row][column + 1].isAlive();
+    }
+
+    private boolean leftCellExistsAndIsAlive(int row, int column) {
+        return column > 0 && getGrid()[row][column - 1].isAlive();
+    }
+
+    private boolean upperCellExistsAndIsAlive(int row, int column) {
+        return row > 0 && getGrid()[row - 1][column].isAlive();
+    }
+
+    private boolean upperRightCellExistsAndIsAlive(int row, int column) {
+        return row > 0 && column != ammountOfRowsAndColumns - 1 && getGrid()[row - 1][column + 1].isAlive();
+    }
+
+    private boolean upperLeftCellExistsAndIsAlive(int row, int column) {
+        return row > 0 && column > 0 && getGrid()[row - 1][column - 1].isAlive();
     }
 }
